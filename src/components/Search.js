e660473cd2481063b33302af3bff5ada55952c32
Matthew Giles <matty92g@hotmail.co.uk>
2023-04-04 00:19:52 +0100

    getImage.js Axios request and Search.js handleSubmit in place

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,29 @@
 import { useState } from "react";
 import React from "react";
 import "../styles/search.css";
+import getImages from "../requests/getImages";
 
 const Search = () => {
   const [value, setValue] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
+
   return (
-    <>
-      <form className="search-form">
+    <div className="search">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           className="search-input"
           type="text"
           onChange={(e) => setValue(e.target.value)}
         />
-        <button className="search-button" type="submit">
+        <button className="search-btn" type="submit">
           GO!
         </button>
       </form>
-    </>
+    </div>
   );
 };
 
