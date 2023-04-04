2f83e3859192377444d6d7bfef4dd58819d252e6
Matthew Giles <matty92g@hotmail.co.uk>
2023-04-05 00:00:10 +0100

    Search Results default value set in hook

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,15 +3,13 @@ import React from "react";
 import "../styles/searchresults.css";
 
 const SearchResults = () => {
-  const [value, setValue] = useState();
+  const defaultLink =
+    "https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80";
+  const [value, setValue] = useState(defaultLink);
   return (
     <>
       <p>Search Results</p>
-      <img
-        className="search-results"
-        src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        alt="searchResults"
-      />
+      <img className="search-results" src={value} alt="searchResults" />
     </>
   );
 };
