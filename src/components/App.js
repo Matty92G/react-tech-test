e93d0e53dffacac993702db7d0a04c52dde18988
Matthew Giles <matty92g@hotmail.co.uk>
2023-04-05 20:59:18 +0100

    Result condition if no result for search added. currently only shows No Result as Search doesn't update image useState

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ const App = () => {
         src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
         alt="nasaLogo"
       />
+      {console.log(searchResults)}
       <Search setSearchResults={setSearchResults} />
       <SearchResults />
     </div>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,16 +2,20 @@ import { useState } from "react";
 import React from "react";
 import "../styles/searchresults.css";
 
-const SearchResults = () => {
+const SearchResults = (results) => {
   const defaultLink =
     "https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80";
   const [image, setImage] = useState(defaultLink);
-  return (
-    <>
-      <p>Search Results</p>
-      <img className="search-results" src={image} alt="searchResults" />
-    </>
-  );
+  if (!results.length) {
+    return <p>No results</p>;
+  } else {
+    return (
+      <>
+        <p>Search Results</p>
+        <img className="search-results" src={image} alt="searchResults" />
+      </>
+    );
+  }
 };
 
 export default SearchResults;
